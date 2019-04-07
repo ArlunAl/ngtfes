ef5fbd38353dc169882bbfc74255ca2fab5829aa
yayo256 <>
2019-04-07 10:15:39 +0900

    vue first commit

---
+++ vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <Top />
+    </div>
+</template>
+
+<script>
+import Top from "./template/Top.vue"
+
+export default {
+    data() {
+        return {
+        }
+    },
+    components: {
+        Top
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: [
+            './static/reset.css',
+            './src/main.js',
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        scss: 'vue-style-loader!css-loader!sass-loader',
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                loader: "style-loader!css-loader!sass-loader"
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader",
+            },
+            {
+                test: /\.(jpg|png)$/,
+                loaders: 'url-loader'
+            },
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "./src/index.html")
+        }),
+    ],
+}
