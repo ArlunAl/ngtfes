fc626b02e6180beadb294e457c444d41155915c7
yayo256 <>
2019-04-08 00:23:22 +0900

    Menu の Icon を作成

---
+++ vue/src/components/HomeMenu.vue
@@ -0,0 +1,26 @@
+<template>
+    <MenuIcon 
+        :isOpen      ="isOpen"
+        :toggleIsOpen="toggleIsOpen"
+    />
+</template>
+
+<script>
+import MenuIcon from "./HomeMenuIcon.vue"
+
+export default {
+    data() {
+        return {
+            isOpen: false,
+        }
+    },
+    components: {
+        MenuIcon,
+    },
+    methods: {
+        toggleIsOpen() {
+            this.isOpen = !this.isOpen
+        }
+    },
+}
+</script>
---
+++ vue/src/components/HomeMenuIconBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <!--  
+        QUESITON: このコンポーネントは必要か？
+        分割のしすぎな気もする 
+    -->
+    <div class="home-menu-icon-bar" />
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.home-menu-icon-bar {
+    background-color: $theme;
+    height: 2px;
+    width: 34px;
+    transition: all .2s;
+    border-radius: 1px;
+    transform-origin: left center;
+}
+</style>
