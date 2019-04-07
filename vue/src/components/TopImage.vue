81ec1cc6bf1a8f076e393ed8e73d637b1a4065d0
yayo256 <>
2019-04-07 22:22:34 +0900

    Fix: Top Page

---
+++ vue/src/components/TopImage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="top-image" />
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.top-image {
+    &::before {
+        content: "";
+        display: block;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: -10;
+        height: 100vh;
+        width: 100%;
+        background: url(../assets/top_image.png) center/cover no-repeat;
+    }
+}
+</style>
