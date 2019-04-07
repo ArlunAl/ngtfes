697b59d69cb8eca70e541ce76e30df1df0d3c613
yayo256 <>
2019-04-08 00:23:07 +0900

    Top部分(画像とタイトル)を作成

---
+++ vue/src/components/HomeTop.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home-top">
+        <TopImage />
+        <TopHeading />
+    </div>
+</template>
+
+<script>
+import TopHeading from "./HomeTopHeading.vue"
+import TopImage from "./HomeTopImage.vue"
+
+export default {
+    components: {
+        TopHeading,
+        TopImage,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ vue/src/components/HomeTopImage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home-top-image" />
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.home-top-image {
+    &::before {
+        content: "";
+        display: block;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: -10;
+        height: 100vh;
+        width: 100%;
+        background: url(../assets/top_image.png) center/cover no-repeat;
+    }
+}
+</style>
