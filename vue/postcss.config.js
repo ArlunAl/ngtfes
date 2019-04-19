544d13fb630c9777b1c4155d673d49797109f861
yayo256 <>
2019-04-19 10:06:01 +0900

    vue, flocss move to _yayo

--- vue/postcss.config.js
+++
@@ -1,6 +0,0 @@
-// QUESTION: このファイルは何？どのように何を記述したら良い？ 
-module.exports = (ctx) => ({
-    plugins: [
-        require('autoprefixer'),
-    ]
-});
--- vue/src/page/PageInterview.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        インタビューページ
-    </div>
-</template>
